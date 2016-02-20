a7f9839f7b75affff450d8f0e1bdb7accfeb1660
Lucas <lucas@agtlucas.com>
2016-02-20 11:35:27 -0200

    Update ViewController

--- STimeTracker/ViewController.swift
+++ STimeTracker/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    let projects = store.projects
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +21,48 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return projects.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ProjectCell") as! ProjectCell
+        cell.project = projects[indexPath.row]
+        return cell
+    }
 
 }
 
+class ProjectCell: UITableViewCell {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var elapsedTimeLabel: UILabel!
+    @IBOutlet var activityButton: UIButton!
+    
+    var project: Project? {
+        didSet {
+            guard let project = project else { return }
+            nameLabel.text = project.name
+            if project.currentActivity != nil {
+                elapsedTimeLabel.text = "⌚️"
+                activityButton.setTitle("Stop", forState: .Normal)
+            } else {
+                elapsedTimeLabel.text = NSDateComponentsFormatter().stringFromTimeInterval(project.elapsedTime)
+                activityButton.setTitle("Start", forState: .Normal)
+            }
+        }
+    }
+    
+    @IBAction func activityButtonTapped() {
+        guard let project = project else { return }
+        if project.currentActivity == nil {
+            // TODO: Start a new Activity
+        } else {
+            // TODO: Complete the Activity
+        }
+    }
+}
